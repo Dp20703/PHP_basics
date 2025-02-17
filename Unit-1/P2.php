2798e714c9a80171368a3958825f94f7a6f77d01
dp <codewithdp2073@gmail.com>
2025-02-17 15:57:42 +0530

    first commit

---
+++ Unit-1/P2.php
@@ -0,0 +1,9 @@
+<?php 
+echo("below line is in commet");
+//echo("this line is in single lien comment"):
+print "hello asia";
+print 100;
+/* $age=23;
+print "Payal patel age is".$age;
+print("this is multiline commet in program"); */
+?>
